898c665ddc1adb0ab14ee230283031aa9ab9f88f
Davidxys <xuyongsh@outlook.com>
2015-08-20 15:53:02 +0800

    add test

--- js/Views/TodoView.js
+++ js/Views/TodoView.js
@@ -6,8 +6,10 @@ app.TodoView = Backbone.View.extend({
 	itemTemplate: _.template($('#item-template').html()),
 
 	render: function() {
-		this.$el.html(this.itemTemplate(this.model.attributes));
+
+        this.$el.html(this.itemTemplate(this.model.attributes));
 		return this;
+
 	}
 
 });
